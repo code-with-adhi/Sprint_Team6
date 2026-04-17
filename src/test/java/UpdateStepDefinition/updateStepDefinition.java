51a6cf07acb500574a3fa05f3b3fbf8f563dcbb7
Prithibha <prithibhapalani@gmail.com>
2026-04-17 13:14:02 +0530

    excel utility

---
+++ src/test/java/UpdateStepDefinition/updateStepDefinition.java
@@ -0,0 +1,168 @@
+package UpdateStepDefinition;
+
+import io.cucumber.java.en.*;
+import io.restassured.RestAssured;
+import io.restassured.response.Response;
+import io.restassured.http.ContentType;
+
+import static io.restassured.RestAssured.*;
+import static org.testng.Assert.*;
+
+import org.testng.annotations.Test;
+
+import ExcelUtility.excelUtility;
+
+public class updateStepDefinition {
+
+    excelUtility eUtil = new excelUtility();
+
+    String token;
+    int bookingId = 1; 
+    Response response;
+
+    @Given("Generate valid authentication token")
+    @Test
+    public void generate_valid_token() {
+
+        RestAssured.baseURI = "https://restful-booker.herokuapp.com";
+
+        String authBody = "{ \"username\":\"admin\", \"password\":\"password123\" }";
+
+        response = given()
+                .contentType(ContentType.JSON)
+                .body(authBody)
+                .post("/auth");
+
+        token = response.jsonPath().getString("token");
+    }
+
+    @When("Send PUT request with complete valid body")
+    @Test
+    public void send_put_request_with_complete_valid_body() throws Exception {
+
+        String firstname = eUtil.getDataFromExcel("Sheet1", 1, 0);
+        String lastname  = eUtil.getDataFromExcel("Sheet1", 1, 1);
+        String totalprice = eUtil.getDataFromExcel("Sheet1", 1, 2);
+
+        String body = "{\n" +
+                "  \"firstname\": \"" + firstname + "\",\n" +
+                "  \"lastname\": \"" + lastname + "\",\n" +
+                "  \"totalprice\": " + totalprice + ",\n" +
+                "  \"depositpaid\": true,\n" +
+                "  \"bookingdates\": {\n" +
+                "    \"checkin\": \"2026-05-01\",\n" +
+                "    \"checkout\": \"2026-05-10\"\n" +
+                "  },\n" +
+                "  \"additionalneeds\": \"Breakfast\"\n" +
+                "}";
+
+        response = sendPutRequest(body, bookingId);
+    }
+
+    @When("Send PUT request without lastname field")
+    @Test
+    public void put_without_lastname() {
+
+        String body = "{\n" +
+                "  \"firstname\": \"Sally\",\n" +
+                "  \"totalprice\": 2000,\n" +
+                "  \"depositpaid\": false,\n" +
+                "  \"bookingdates\": {\n" +
+                "    \"checkin\": \"2026-06-01\",\n" +
+                "    \"checkout\": \"2026-06-15\"\n" +
+                "  },\n" +
+                "  \"additionalneeds\": \"Dinner\"\n" +
+                "}";
+
+        response = sendPutRequest(body, bookingId);
+    }
+
+    @When("Send PUT request with completely different values")
+    @Test
+    public void put_all_fields_changed() {
+
+        String body = "{\n" +
+                "  \"firstname\": \"Sally\",\n" +
+                "  \"lastname\": \"Vidhya\",\n" +
+                "  \"totalprice\": 2000,\n" +
+                "  \"depositpaid\": false,\n" +
+                "  \"bookingdates\": {\n" +
+                "    \"checkin\": \"2026-06-01\",\n" +
+                "    \"checkout\": \"2026-06-15\"\n" +
+                "  },\n" +
+                "  \"additionalneeds\": \"Dinner\"\n" +
+                "}";
+
+        response = sendPutRequest(body, bookingId);
+    }
+
+    @When("Send PUT request updating booking dates")
+    @Test
+    public void put_update_dates() {
+
+        String body = "{\n" +
+                "  \"firstname\": \"Jim\",\n" +
+                "  \"lastname\": \"Smith\",\n" +
+                "  \"totalprice\": 832,\n" +
+                "  \"depositpaid\": true,\n" +
+                "  \"bookingdates\": {\n" +
+                "    \"checkin\": \"2026-07-01\",\n" +
+                "    \"checkout\": \"2026-07-20\"\n" +
+                "  }\n" +
+                "}";
+
+        response = sendPutRequest(body, bookingId);
+    }
+
+    @When("Send PUT request without lastname but with valid price and deposit")
+    @Test
+    public void put_missing_lastname_price_valid() {
+
+        String body = "{\n" +
+                "  \"firstname\": \"John\",\n" +
+                "  \"totalprice\": 3000,\n" +
+                "  \"depositpaid\": false,\n" +
+                "  \"bookingdates\": {\n" +
+                "    \"checkin\": \"2026-08-01\",\n" +
+                "    \"checkout\": \"2026-08-05\"\n" +
+                "  },\n" +
+                "  \"additionalneeds\": \"WiFi\"\n" +
+                "}";
+
+        response = sendPutRequest(body, bookingId);
+    }
+
+    @When("Send PUT request with non existing booking id")
+    @Test
+    public void put_non_existing_id() {
+
+        String body = "{\n" +
+                "  \"firstname\": \"John\",\n" +
+                "  \"lastname\": \"Doe\",\n" +
+                "  \"totalprice\": 1200,\n" +
+                "  \"depositpaid\": true,\n" +
+                "  \"bookingdates\": {\n" +
+                "    \"checkin\": \"2026-05-01\",\n" +
+                "    \"checkout\": \"2026-05-10\"\n" +
+                "  },\n" +
+                "  \"additionalneeds\": \"Breakfast\"\n" +
+                "}";
+
+        response = sendPutRequest(body, 999999);
+    }
+    
+    public Response sendPutRequest(String body, int id) {
+
+        return given()
+                .contentType(ContentType.JSON)
+                .cookie("token", token)
+                .body(body)
+                .put("/booking/" + id);
+    }
+
+    @Then("Validate status code should be {int}")
+    @Test
+    public void validate_status_code(int expectedStatusCode) {
+        assertEquals(response.getStatusCode(), expectedStatusCode);
+    }
+}
---
+++ src/test/resources/UpdateFeature/updateFeature.feature
@@ -0,0 +1,36 @@
+#Author: Prithibha
+Feature: Validate Complete Updation of Booking using PUT
+
+  Background:
+    Given Generate valid authentication token
+    
+
+  @TC22
+  Scenario: TC_22 Validate update without mandatory field lastname
+    When Send PUT request without lastname field
+    Then Validate status code should be 400
+
+  @TC23
+  Scenario: TC_23 Validate complete update with valid data
+    When Send PUT request with complete valid body
+    Then Validate status code should be 200
+
+  @TC24
+  Scenario: TC_24 Validate update with all fields changed
+    When Send PUT request with completely different values
+    Then Validate status code should be 200
+
+  @TC25
+  Scenario: TC_25 Validate update with valid booking dates
+    When Send PUT request updating booking dates
+    Then Validate status code should be 200
+
+  @TC26
+  Scenario: TC_26 Validate update without lastname but valid totalprice & depositpaid
+    When Send PUT request without lastname but with valid price and deposit
+    Then Validate status code should be 400
+
+  @TC27
+  Scenario: TC_27 Validate update with non-existing booking ID
+    When Send PUT request with non existing booking id
+    Then Validate status code should be 404

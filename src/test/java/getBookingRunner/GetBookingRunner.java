c466d69cb3e7b56c0f9369413212179aa825d552
rajmohan2004 <rajmohanthangaraju0211@gmail.com>
2026-04-20 09:18:35 +0530

    Create and Get methods are updated

--- src/test/java/getBookingRunner/GetBookingRunner.java
+++ src/test/java/getBookingRunner/GetBookingRunner.java
@@ -1,8 +1,13 @@
 package getBookingRunner;
 
-import io.cucumber.testng.AbstractTestNGCucumberTests;
-import io.cucumber.testng.CucumberOptions;
+//import io.cucumber.testng.AbstractTestNGCucumberTests;
+//import io.cucumber.testng.CucumberOptions;
 
+import org.junit.runner.RunWith;
+import io.cucumber.junit.Cucumber;
+import io.cucumber.junit.CucumberOptions;
+
+@RunWith(Cucumber.class)
 @CucumberOptions(
 	    features = "src/test/resources/GetBookingfeature/GetFeatureFile.feature",
 	    glue = "GetBookingByID",
@@ -10,6 +15,12 @@ import io.cucumber.testng.CucumberOptions;
 	    monochrome = true
 	)
 
+public class GetBookingRunner {
+
+}
+
+/*
 public class GetBookingRunner extends AbstractTestNGCucumberTests {
 
 }
+*/

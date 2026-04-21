551b1b08e2f070e6987c7760fe7cbd533a1e3bbd
rajmohan2004 <rajmohanthangaraju0211@gmail.com>
2026-04-21 09:10:55 +0530

    Hooks file created

---
+++ src/test/java/hooks/Hooks.java
@@ -0,0 +1,27 @@
+package hooks;
+
+import io.cucumber.java.Before;
+
+import java.io.IOException;
+
+import file_utility.FileUtility;
+import io.cucumber.java.After;
+import io.restassured.RestAssured;
+
+public class Hooks {
+
+    @Before
+    public void beforeScenario() throws IOException {
+        System.out.println("=== Test Started ===");
+
+        // Set Base URI globally
+        // RestAssured.baseURI = "https://restful-booker.herokuapp.com";
+        FileUtility fLib = new FileUtility();
+        RestAssured.baseURI = fLib.getDataFromPropertiesFile("baseurl");
+    }
+
+    @After
+    public void afterScenario() {
+        System.out.println("=== Test Finished ===");
+    }
+}
